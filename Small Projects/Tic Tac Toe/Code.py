afbc45f9ca82df522a85674434dd6bebe943e7b4
Khaled Mahmoud <46223389+Khaled-Mahmmoud@users.noreply.github.com>
2024-08-12 16:26:13 +0300

    Python - Object Oriented Programming

--- Small Projects/Tic Tac Toe/Code.py
+++ Small Projects/Tic Tac Toe/Code.py
@@ -1,3 +1,9 @@
+import os
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear") 
+    # cls for Windows and clear for Linux and Mac
+
 class Player():
     def __init__(self):
         self.name = ""
@@ -18,8 +24,7 @@ class Player():
                 self.symbol = symbol.upper()
                 break
             print("Invalid symbol.")
-
-
+    
 class Menu():
     def display_main_menu(self):
         menu_text = """
@@ -31,7 +36,7 @@ class Menu():
     
     def display_endgame_menu(self):
         menu_text = """
-        Welcome to Tic Tac Toe!
+        Game Over!
         1. Restart Game
         2. Quit Game
         Enter your choice (1 or 2): """
@@ -46,7 +51,7 @@ class Menu():
     
 class Board():
     def __init__(self):
-        self.board = [str(i) for i in range(1, 10)]
+        self.board = [str(i) for i in range(1, 10)]  # ["1","2","3","4","5","6","7","8","9"]
     
     def display_board(self):
         for i in range(0, 9, 3):
@@ -82,17 +87,70 @@ class Game():
             self.quit_game()
     
     def setup_players(self):
-        for player in self.players:
+        for number, player in enumerate(self.players, start = 1):
+            clear_screen()
+            print(f"Player {number}, Enter your details:")
             player.choose_name()
             player.choose_symbol()
-    
+            
     def play_game(self):
-        pass
+        while True:
+            clear_screen()
+            self.play_turn()
+            if self.check_win() or self.check_draw():
+                clear_screen()
+                self.board.display_board()
+                if(self.check_win()):
+                    print(f"Congratulations, {self.players[self.current_player_index].name} won!")
+                else:
+                    print("Draw!")
+                choice = self.menu.display_endgame_menu()
+                if choice == "1":
+                    self.restart_game()
+                else:
+                    self.quit_game()
+                    break
+            self.switch_player()
+    
+    def play_turn(self):
+        player = self.players[self.current_player_index]
+        self.board.display_board()
+        print(f"{player.name}'s turn ({player.symbol})")
+        while True:
+            try:
+                cell_choice = int(input("Choose a cell (1-9): "))
+                if 1 <= cell_choice <= 9 and self.board.update_board(cell_choice,player.symbol):
+                    break
+                else:
+                    print("Invalid move.")
+            except ValueError:
+                print("Please enter a number between 1 and 9.")
+        
+
+    def switch_player(self):
+        self.current_player_index = 1 - self.current_player_index
+    
+    def check_win(self):
+        win_combinations = [
+            [0,1,2], [3,4,5], [6,7,8],   # rows
+            [0,3,6], [1,4,7], [2,5,8],   # columns
+            [0,4,8], [2,4,6]             # diagonals
+        ]
+        for combo in win_combinations:
+            if(self.board.board[combo[0]]==self.board.board[combo[1]]==self.board.board[combo[2]]):
+                return True 
+        return False
+
+    def check_draw(self):
+        return all(not cell.isdigit() for cell in self.board.board)
+
+    def restart_game(self):
+        self.board.reset_board()
+        self.current_player_index = 0
+        self.play_game()
 
     def quit_game(self):
-        pass
-
+        print("Thank you for Playing!")
 
 game = Game()
 game.start_game()
-
